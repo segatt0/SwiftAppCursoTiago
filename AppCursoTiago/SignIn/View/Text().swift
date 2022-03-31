4541ec7662a2c8ad369264ed10d81f6b97b4237a
segatt0 <anabellesegatto@hotmail.com>
2022-03-31 13:11:47 -0300

    add scrollview

--- AppCursoTiago/SignIn/View/Text().swift
+++ AppCursoTiago/SignIn/View/Text().swift
@@ -13,26 +13,45 @@ struct SignInView: View {
     @State var email = " "
     @State var password = " "
     
+    @State var navigationHidden = true
+    
+    @State var action: Int? = 0
+    
     var body: some View {
-        VStack(alignment: .center) {
-            Image("logo")
-                .resizable()
-                .scaledToFit()
-                .padding(.horizontal, 48)
-                .background(Color.red)
+        NavigationView{
+            ScrollView(showsIndicators: true) {
+                VStack(alignment: .center, spacing: 20) {
+                 Spacer(minLength: 36)
+                    
+            VStack(alignment: .center) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 48)
+//                    .background(Color.red)
+                
+                Text("Login")
+                    .foregroundColor(.orange)
+                    .font(Font.system(.title).bold())
+                    .padding(.bottom, 8)
+//                    .background(Color.green)
+                
+                numberField
+                passwordField
+                enterButton
+                registerLink
+            }
+                }
+                  
             
-            Text("Login")
-                .foregroundColor(.orange)
-                .font(Font.system(.title).bold())
-                .padding(.bottom, 8)
-                .background(Color.green)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(.horizontal, 32)
+                .background(Color.white)
+                .navigationBarTitle("login", displayMode: .inline)
+                .navigationBarHidden(navigationHidden)
             
-            numberField
-            passwordField
-            enterButton
-            registerLink
-            
-    }
+        }
+       
     }
 }
 
@@ -57,7 +76,7 @@ extension SignInView {
         Button("Entrar"){
             
         }
-        .background(Color.purple) // i am putting aleatory colors because i want to see the size of view
+//        .background(Color.purple) // i am putting aleatory colors because i want to see the size of view
     }
 }
 
@@ -68,7 +87,19 @@ extension SignInView {
                 .foregroundColor(.gray)
                 .padding(.top, 48)
             
-        }
+            
+            
+            ZStack {
+                NavigationLink(
+                    destination: Text("tela de cadastro"), tag: 1, selection: $action, label: {EmptyView()})
+                
+                Button("Realize seu cadastro") {
+                    self.action = 1 }
+                
+                
+            }
+            }
+        
             
         
     }
