9f87a3543681e199146959c5b56f20d1f233f0e3
segatt0 <anabellesegatto@hotmail.com>
2022-04-04 18:03:57 -0300

    register screen

--- AppCursoTiago/SignUp/View/SignUpView.swift
+++ AppCursoTiago/SignUp/View/SignUpView.swift
@@ -18,13 +18,93 @@ struct SignUpView: View {
     
     
     var body: some View {
-        Text("hello world")
+        
+        VStack(alignment: .leading, spacing: 8) {
+            
+            Text("Cadastro")
+                .foregroundColor(.blue)
+                .font(Font.system(.title).bold())
+                .padding(.bottom, 8)
+            
+            fullNameField
+            emailField
+            passwordField
+            documentField
+            phoneField
+            birthdayField
+            saveButton
+            
+            
+        }
     }
 }
+
+
+extension SignUpView {
+    var fullNameField: some View {
+        TextField("", text: $fullName)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var emailField: some View {
+        TextField("", text: $email)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var passwordField: some View {
+        SecureField("", text: $password)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var documentField: some View {
+        TextField("", text: $document)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var phoneField: some View {
+        TextField("", text: $phone)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var birthdayField: some View {
+        TextField("", text: $birthday)
+            .border(Color.black)
+    }
+}
+
+
+
+extension SignUpView {
+    var saveButton: some View {
+        Button("Realize seu cadastro") {
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
         SignUpView()
     }
 }
-  
+
